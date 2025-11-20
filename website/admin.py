7bdae0a8c013a33c5e6d56e434bb6d1a4e31b455
purushottam <purushottam@menteso.com>
2025-11-20 12:15:32 +0530

    udpate admin fields

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from .models import Service, CaseStudy, Doctor, Testimonial
 
-admin.site.register(Service)
-admin.site.register(CaseStudy)
-admin.site.register(Doctor)
-admin.site.register(Testimonial)
+prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','is_active','created_at')
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    
+@admin.register(CaseStudy)
+class CaseStudyAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','is_active','created_at')
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('name','specialization','experience_years','rating','created_at','is_active')
+    search_fields = ('name','specialization', 'experience_years')
+    prepopulated_fields = {'slug': ('name',)}
+    
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('name','created_at','is_active' )
+    search_fields = ('name',)
+    
